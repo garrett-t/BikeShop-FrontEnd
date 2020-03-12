68e81ad7fdd7b65f87ea45672665e5bde6ba29f1
garrett-t <LANDONG@ETSU.EDU>
2020-03-11 21:52:00 -0400

    Established simple communication with Monitoring API

---
+++ BikeShop_FrontEnd/Controllers/MonitoringController.cs
@@ -0,0 +1,40 @@
+﻿using BikeShop_FrontEnd.Models.Monitoring;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeShop_FrontEnd.Controllers
+{
+    public class MonitoringController : Controller
+    {
+        // GET: Monitoring
+        public ActionResult Index()
+        {
+            IEnumerable<Metrics> metrics = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://dahkm.azurewebsites.net/api/");
+                var responseTask = client.GetAsync("metricscontroller");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Metrics>>();
+                    readTask.Wait();
+
+                    metrics = readTask.Result;
+                }
+                else
+                {
+                    metrics = Enumerable.Empty<Metrics>();
+                    ModelState.AddModelError(string.Empty, "Server error");
+                }
+            }
+            return View(metrics);
+        }
+    }
+}
---
+++ BikeShop_FrontEnd/Models/Monitoring/Metrics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BikeShop_FrontEnd.Models.Monitoring
+{
+    public class Metrics
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int ClicksAM { get; set; }
+        public int ClicksAfternoon { get; set; }
+        public int ClicksPM { get; set; }
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal ConversionRate { get; set; }
+    }
+}
