c4ad6634a9ea65a6e793e36c1dbb8437a75d8dbf
garrett-t <LANDONG@ETSU.EDU>
2020-03-12 15:45:43 -0400

    Communication with monitoring teams API, get still works but no post

--- BikeShop_FrontEnd/Controllers/MonitoringController.cs
+++ BikeShop_FrontEnd/Controllers/MonitoringController.cs
@@ -36,5 +36,56 @@ namespace BikeShop_FrontEnd.Controllers
             }
             return View(metrics);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Metrics metrics)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://dahkm.azurewebsites.net/api/metricscontroller");
+
+                var postTask = client.PostAsJsonAsync<Metrics>("metricscontroller", metrics);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server error.");
+            return View(metrics);
+        }
+
+        public ActionResult Customers()
+        {
+            IEnumerable<Customer> customers = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://dahkm.azurewebsites.net/api/");
+                var responseTask = client.GetAsync("customercontroller");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Customer>>();
+                    readTask.Wait();
+
+                    customers = readTask.Result;
+                }
+                else
+                {
+                    customers = Enumerable.Empty<Customer>();
+                    ModelState.AddModelError(string.Empty, "Server error");
+                }
+            }
+            return View(customers);
+        }
     }
 }
