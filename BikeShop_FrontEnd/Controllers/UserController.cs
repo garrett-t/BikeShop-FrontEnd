f3ed4e1a6d550e4eff16c8c4ac2cf3ea840e272d
garrett-t <LANDONG@ETSU.EDU>
2020-04-08 16:18:26 -0400

    Added account system

---
+++ BikeShop_FrontEnd/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using BikeShop_FrontEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BikeShop_FrontEnd.Controllers
+{
+    //[Authorize]
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            using (SE2Entities context = new SE2Entities())
+            {
+                bool IsValidUser = context.Users.Any(user => user.UserName.ToLower() == model.UserName.ToLower()
+                && user.UserPassword == model.Password);
+
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid Username or Password");
+                return View();
+            }
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User model)
+        {
+            using (SE2Entities context = new SE2Entities())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
